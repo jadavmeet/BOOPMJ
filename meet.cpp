1998a7031f9c124e1a48506bac3b9a9f4825af60
unknown <meetjadav070@gmail.com>
2023-06-30 16:41:26 +0530

    first commit

---
+++ meet.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"MEET JADAV"<<endl;
+	cout<<"PUBG PLAYER"<<endl;
+	return 0;
+}
---
+++ mj.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"MEET JADAV"<<endl;
+	cout<<"PUBG PLAYER"<<endl;
+	cout<<"226270307036"<<endl;
+	return 0;
+}
